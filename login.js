68f0e599702f7249472bd62313554f3b24dc8164
fernando <ferblanco@gmail.com>
2022-10-27 23:10:50 -0300

    agrego home y modifico loginJs donde hago el redireccionamiento

---
+++ login.js
@@ -0,0 +1,21 @@
+//valida si el usuario y pass son correctos 
+function validate() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    if (username = "admin" && password == "lala") {
+        window.location.href = "home.html"; 
+        return false; 
+    } else {
+        alert("usuario o password incorrectos");
+    }
+}
+
+let button = document.getElementById("button");
+
+button.addEventListener("click", (e)=> {
+    e.preventDefault();
+    validate();
+})
+
+
+
